811feb26aa9090601138c6622b9fdfd32ea702d9
Viktoryia Vysotskaya <radevich.vika2014@gmail.com>
2023-04-22 08:46:20 +0200

    Add images in script.js

--- src/js/script.js
+++ src/js/script.js
@@ -85,6 +85,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion() {
@@ -151,6 +152,17 @@
             if (option.default) {
               // reduce price variable
               price -= option.price;
+            }
+          }
+          // find image with class .paramId-optionId
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+          if (optionImage) {
+            if (optionSelected) {
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+            }
+            else {
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
             }
           }
         }
